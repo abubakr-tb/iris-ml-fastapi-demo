93de355817e393f5bf5863a4ed604fa598266079
Abu Bakr <106076734+abubakr-tb@users.noreply.github.com>
2022-06-03 00:04:08 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from array import array
+import pickle
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 import numpy as np
-from fastapi import FastAPI, Form, Body
+from fastapi import FastAPI, Body
 from pydantic import BaseModel
-import ml
-import pandas as pd
-from typing import List, Union
+from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder
+from typing import List
+from tensorflow.keras.models import load_model
 from fastapi.encoders import jsonable_encoder
-
 
 app = FastAPI()
 
@@ -19,85 +23,26 @@ class Input(BaseModel):
 class item(BaseModel):
     items: List[Input]
 
-# class Response(BaseModel):
-#     loss: float
-#     accuracy: float
-#     actual: List[str]=[]
-#     predicted: List[str]=[]
-
 @app.get("/")
 async def root():
     return {"message": "Such ML much wow"}
 
+def get_model():
+    return load_model("mymodel.h5")
+
 @app.post("/predictions")
 async def prediction(input: dict = Body(...)):
-    
-    ml.load_file()
 
-    # allSepalLength, allSepalWidth, allPetalLength, allPetalWidth =[],[],[],[]
-    # print(type(storage))
-    # print(input)
     body = input['items']
-    # print(body)
-    print([np.array(item.values()) for item in body])
 
-    # print(body)
+    X = np.array([list(input.values()) for input in body])
 
-    [
-        [5.1, 3.5, 1.4, 0.2],
-        [5.1, 3.5, 1.4, 0.2]
-    ]
+    model = get_model()
 
+    y_pred = np.argmax(model.predict(X), axis=1)
 
-    return body
-    # data = pd.DataFrame.from_dict(body)
-    # data.head()
+    encoder = pickle.load(open('enc.pkl', 'rb'))
 
-    # for things in body:
-    #     data = pd.DataFrame.from_dict(things)
-    # print(data)
-        # data = things.items()
-        # listy = list(data)
-        # arr = np.array(listy)
-        # print(arr)
+    labels1 = encoder.inverse_transform(y_pred[:]) 
 
-    # X = data.iloc[:,0:4].values
-    # y = data.iloc[:,1].values
-    # print(X[0:5])
-
-
-    # print(y[0:5])
-    # y = ml.encoder_transform(Y)
-
-    # final = ml.train_test(X,y)
-
-    # return input
-    #     allSepalLength.append(things['SepalLength'])
-    #     allSepalWidth.append(things['SepalWidth'])
-    #     allPetalLength.append(things['PetalLength'])
-    #     allPetalWidth.append(things['PetalWidth'])
-    #     np1=np.array(allSepalLength)
-    #     np2=np.array(allSepalWidth)
-    #     np3=np.array(allPetalLength)
-    #     np4=np.array(allPetalWidth)
-    #     df = pd.DataFrame(data=[np1, np2, np3, np4]).T
-    # # print(df)
-    
-    # print(np1)
-    # print(np2)
-    # print(np3)
-    # print(np4)
-            
-    # print(allSepalLength)
-    # print(allSepalWidth)
-    # print(allPetalLength)
-    # print(allPetalWidth)
-    # for stuffs in storage:
-    #     # print(stuffs[1])
-    #     for things in stuffs:
-    #         # print(things)
-    #         for goods in things:
-    #             print(goods)
-    # print(json_item_data)
-    # print(type(input))
-    # return input
+    return labels1
--- ml.py
+++ ml.py
@@ -1,56 +1,34 @@
-import pandas as pd, pprint
+import pickle
+import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from tensorflow.python import layers
 import numpy as np
 
-def load_file():
-    dfile = "Iris.csv"
-    df = pd.read_csv(dfile)
-    df.head()
+dfile = "Iris.csv"
+df = pd.read_csv(dfile)
+df.head()
 
-    X = df.iloc[:,0:4].values
-    Y = df.iloc[:,4].values
-    # print(type(X))
-    # print(type(Y))
-    return X, Y
+X = df.iloc[:,0:4].values
+Y = df.iloc[:,4].values
 
-def encoder_transform(Y):
-    encoder = LabelEncoder()
-    y1 = encoder.fit_transform(Y)
-    y = pd.get_dummies(y1).values
+encoder = LabelEncoder()
+y1 = encoder.fit_transform(Y)
 
-    return y
+pickle.dump(encoder, open('enc.pkl', 'wb'))
 
-def train_test(X, y):
-    Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, y, test_size=0.2, random_state=0)
-    model = tf.keras.Sequential([
-        tf.keras.layers.Dense(10, activation='relu'),
-        tf.keras.layers.Dense(10, activation='relu'),
-        tf.keras.layers.Dense(3, activation='softmax')
-        ])
+y = pd.get_dummies(y1).values
 
-    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, y, test_size=0.2, random_state=0)
+model = tf.keras.Sequential([
+tf.keras.layers.Dense(10, activation='relu'),
+tf.keras.layers.Dense(10, activation='relu'),
+tf.keras.layers.Dense(3, activation='softmax')
+])
 
-    model.fit(Xtrain, Ytrain, batch_size=50, epochs=100)
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
-    loss, accuracy = model.evaluate(Xtest, Ytest, verbose=0)
-    # print("Test loss: ", loss)
-    # print("Test accuracy: ", accuracy)
+model.fit(Xtrain, Ytrain, batch_size=50, epochs=100)
 
-    y_pred = model.predict(Xtest)
-
-    actual = np.argmax(Ytest, axis=1)
-    predicted = np.argmax(y_pred, axis=1)
-
-    # print(f"Actual: {actual}")
-    # print(f"Predicted: {predicted}")
-
-    return loss, accuracy, actual, predicted
-
-if __name__ == "__main__":
-
-    loaded_result = load_file()
-    encoded_result = encoder_transform(loaded_result[1])
-    train_test_result = train_test(loaded_result[0], encoded_result)
+model.save("mymodel.h5")
